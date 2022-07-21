f8b67b101593e105c4b95c1ce72b4efd361fff71
dsigner7 <69458165+martin-webs@users.noreply.github.com>
2022-07-21 14:11:11 -0700

    updated typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
       fontFamily: {
         custom1: ['Source Sans Pro', 'sans'],
         custom2: ['Courgette', 'sans'],
+        custom3: ['Inter', 'sans'],
+        custom4: ['Secular One', 'sans-serif'],
       },
       boxShadow: {
         'shadow1': '0 0 8px 0px white'

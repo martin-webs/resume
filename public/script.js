caf25d13a8492ebb8ea7d7345281f88a6f92a323
dsigner7 <69458165+martin-webs@users.noreply.github.com>
2022-08-23 22:28:42 -0700

    fixed images

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const tooltip1 = document.getElementById("tooltip1");
 const brushLogo = document.getElementById("brush-logo");
 const thankyou = document.getElementById("thankyou");
-const skillset = document.querySelectorAll("#skillset li");
+const skillsetLi = document.querySelectorAll("#skillset li");
 let mark = "";
 
 /* Show tooltip in the footer */
@@ -23,15 +23,17 @@ function createExclamationMark() {
 
 thankyou.addEventListener("mouseover", createExclamationMark);
 
-/* Show skill in a pop up */
-skillset.forEach((skill, index) =>
+skillsetLi.forEach((skill, index) =>
   skill.addEventListener("click", () => {
-    const divContent = `<div class="absolute top-[18rem] left-0 right-0 m-auto w-[20rem] h-[20rem] overflow-y-scroll bg-[#1e1e1e] p-6  md:w-[35rem] md:top-[20rem]"><i id="close-button" class="fa-solid fa-circle-xmark text-xl text-white float-right"></i> <img src="./img/${index}.jpg" alt="html-code"></div>`;
     const div = document.createElement("div");
-    div.innerHTML = divContent;
     document.body.appendChild(div);
+    div.setAttribute(
+      "class",
+      "absolute top-24 inset-x-1 h-96 overflow-y-scroll p-6 bg-[#1e1e1e] border-4 border-yellow-300 rounded-lg delay-500 md:top-44 md:inset-x-36 md:border-[1rem] md:border-yellow-300 lg:h-2/4"
+    );
+    div.innerHTML = `<i id="close-button" class="fa-solid fa-circle-xmark sticky right-4 top-0 text-xl text-yellow-300 md:top-0"></i><img src="./img/${index}.jpg" alt="${skill.innerText} example" />`;
     /* Remove pop up */
-    const closeBtn = div.childNodes[0].childNodes[1];
+    const closeBtn = div.children[0];
     closeBtn.addEventListener("click", () => {
       div.remove();
     });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,26 @@
 module.exports = {
   plugins: {
-    'postcss-import': {},
-    'tailwindcss': {},
-    'autoprefixer': {},
+    "postcss-import": {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  mode: 'jit',
-  content: [
-    './public/index.html',
-    './src/*.js'
-  ],
+  mode: "jit",
+  content: ["./public/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-        custom1: ['Source Sans Pro', 'sans'],
-        custom2: ['Courgette', 'sans'],
-        custom3: ['Inter', 'sans'],
-        custom4: ['Secular One', 'sans-serif'],
+        custom1: ["Source Sans Pro", "sans"],
+        custom2: ["Courgette", "sans"],
+        custom3: ["Inter", "sans"],
+        custom4: ["Secular One", "sans-serif"],
       },
       boxShadow: {
-        'shadow1': '0 0 8px 0px white'
+        shadow1: "0 0 8px 0px white",
       },
       animation: {
-        'fadeIn': '',
-      }
+        fadeIn: "",
+      },
     },
   },
   plugins: [],
-}
-
-
+};
